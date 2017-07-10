ff2c473b6335df7be05d54844c8f6ce3b73b2812
uuario <uuario@150.162.58.174>
2017-07-10 17:14:07 -0300

    Somente exibe os appId que possuo 5 ou mais cards

--- src/test/java/br/lehmann/steam/api/GamesMatcher.java
+++ src/test/java/br/lehmann/steam/api/GamesMatcher.java
@@ -1,9 +1,13 @@
 package br.lehmann.steam.api;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import org.apache.commons.lang3.mutable.MutableInt;
 
 import br.lehmann.steam.api.badge.Badge;
 import br.lehmann.steam.api.badge.BadgePojo;
@@ -17,6 +21,7 @@ public class GamesMatcher {
 		OwnedGames bot = steamInventory.retriveGames("76561198309542212");
 		Inventory my = steamInventory.retriveInventory("76561198009543117");
 		List<AssetDescription> assets = my.getDescriptions();
+		Map<String, MutableInt> stats = badgeStats(assets);
 		BadgePojo userBadgeInfo = steamInventory.retriveBadges("76561198216279305");
 		List<Badge> badges = userBadgeInfo.getResponse().getBadges();
 		Set<String> nonBadges = new LinkedHashSet<>();
@@ -24,6 +29,10 @@ public class GamesMatcher {
 		Set<String> incompleteSet = new LinkedHashSet<>();
 		for (AssetDescription desc : assets) {
 			String appId = desc.market_hash_name.substring(0, desc.market_hash_name.indexOf('-'));
+			if(stats.get(appId).intValue() < 5) {
+				//ignorando cards que eu possua menos que 5 exemplares
+				continue;
+			}
 			if(nonBadges.contains(appId)) {
 				continue;
 			}
@@ -69,5 +78,19 @@ public class GamesMatcher {
 		for (Integer botGame : botGames) {
 			System.out.println("Bot game: " + botGame);
 		}
+	}
+	
+	private static Map<String, MutableInt> badgeStats(List<AssetDescription> assets) {
+		Map<String, MutableInt> ret = new HashMap<>();
+		for (AssetDescription desc : assets) {
+			String appId = desc.market_hash_name.substring(0, desc.market_hash_name.indexOf('-'));
+			MutableInt counter = ret.get(appId);
+			if(counter == null) {
+				ret.put(appId, new MutableInt(1));
+			} else {
+				counter.increment();
+			}
+		}
+		return ret;
 	}
 }
